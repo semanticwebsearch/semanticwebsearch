821f41fc4cf959b60117e5f5f76d76d4ea19ce5f
stefanhagiu <stefanhagiu@gmail.com>
2015-01-06 23:34:36 +0200

    added : - answers image - redesing layer of answers - added js for layer ... and more

--- src/main/webapp/resources/js/test.js
+++ src/main/webapp/resources/js/test.js
@@ -60,28 +60,78 @@ $('#search').keypress(function(){
     $('.top-header').height(searchHegiht+20);*/
 
     if(!($("#toggled_content").find("#sOptions").length == 1)) {
+
         console.log("detache sOption bar");
+
         $("#sOptions").detach().appendTo("#toggled_content");
 
         $("#displayAs").removeClass("no-display");
         $("#sOptions").removeClass("row");
         $("hr").removeClass("no-display");
+
+        lookingFor();
     }
+
 });
 
+//looking for : check what am i looking for and set the list or gread layout for display of answers
+
+  function lookingFor(){
+
+      var groupt = $("#lookFor input:checkbox");
+      var count = 0;
+
+      $.each(groupt, function(index, value){
+
+          console.log('lookingFor : '+value.name);
+          if(value.checked){
+              count +=1;
+          }
+
+      });
+      console.log('lookingFor count :'+count);
+
+      if(count == 0){
+          //load grid
+          //set all img to checked
+          setLayout("grid");
+      }else if(count == 1){
+          //load list
+          setLayout("list");
+        }else{
+            //load grid
+          setLayout("grid");
+        }
+  };
+function setLayout(name){
+
+    var group = $('#displayAs input:checkbox');
+    var displayAs = '#displayAs input:checkbox#'+name;
+    if($(displayAs).length > 0) {
+        console.log($(displayAs));
+        $("#mainLayout").attr("href", $(displayAs).attr('rel'));
+
+        group.attr("checked",false);
+        $(displayAs).attr("checked",true);
+    }else{
+        console.log("[setLayout(name)] there is no id :" + name);
+    }
+}
 $('.toggler').on('click',function(){
 
     $(this).parent().children().toggle();  //swaps the display:none between the two spans
     $(this).parent().parent().find('#toggled_content').slideToggle('fast');  //swap the display of the main content with slide action
 });
 
+//swap the css format for answers display
 $("input:checkbox").click(function(){
+
     if(this.name == "displayOpt") {
+
         var group = "input:checkbox[name='" + $(this).attr("name") + "']";
+
         $(group).attr("checked",false);
         $(this).attr("checked",true);
-
-        var link = $(this).attr('rel');
 
         $("#mainLayout").attr("href",$(this).attr('rel'));
     }
