7037b9f086461e1f76d8959c1b6dcd1acbadec5c
spac-valentin <spac.valentin@gmail.com>
2015-02-02 23:22:35 +0200

    Content interface

--- src/main/java/rest/model/output/content/Content.java
+++ src/main/java/rest/model/output/content/Content.java
@@ -1,11 +1,8 @@
 package rest.model.output.content;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
 /**
  * Created by valentin.spac on 1/27/2015.
  */
-@XmlRootElement
 public interface Content {
 
 }
